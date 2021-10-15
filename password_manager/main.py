64ca601f9da17201920d8a21dc4bddb4024454d1
antonycja <61600706+antonycja@users.noreply.github.com>
2021-10-16 01:07:58 +0200

    Update main.py

--- password_manager/main.py
+++ password_manager/main.py
@@ -119,7 +119,7 @@ user_label.grid(column=0, row=2)
 user_entry = Entry(width=44)
 user_entry.grid(column=1, row=2, columnspan=2)
 user_label.config(padx=10, pady=5)
-user_entry.insert(0, "cja@gmail.com")
+user_entry.insert(0, "test@mail.com")
 
 password_label = Label(text="Password:")
 password_label.grid(column=0, row=3)
